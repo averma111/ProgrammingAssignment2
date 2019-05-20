2f5e6f220dce4ffa73b4e0488addcbdc35375d7a
averma111 <43510964+averma111@users.noreply.github.com>
2019-05-20 14:08:42 -0700

    Update cachematrix.R
    Add the comments in the program

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,37 @@
 ## CachedMatrix Creation
-## Ashish Verma May-19
+## Ashish Verma May-20-2019
 
 
+##Programming Assignment 2
 
+#Function to create Cache Matrix.
 makeCacheMatrix <- function(x = matrix()) {
   
-  indicator<-NULL
+  indicator<-NULL   ##Assigning NULL to indicator
   set<-function(y){
     x<<-y
     indicator<<-NULL
   }
   get <- function() x
-  setinverse <- function(inverse) indicator <<- inverse
+  setinverse <- function(inverse) indicator <<- inverse ##Assigning inverse to indicator
   getinverse <- function() indicator
-  list(set = set,
+  list(set = set,                            ##In the list preparing the serinverse and gerinverse
        get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
+
+#Function to create the CacheSolve
 cacheSolve <- function(x, ...) {
   value <- x$getinverse()
-  if (!is.null(value)) {
-    message("getting cached data")
+  if (!is.null(value)) {          ##Checking if the value are not NULL
+    message("getting cached data") ##Printing the message
     return(value)
   }
   data <- x$get()
   i <- solve(data, ...)
   x$setinverse(value)
-  value
+  value                ##Returning the value back to fucntion call
   
 }
